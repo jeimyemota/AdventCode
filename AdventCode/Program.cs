9ab655c7b2afa424399c8ea93f2e692496570840
Jeimy Evelyn Mota <jmota@softerware.com>
2022-12-04 13:39:36 -0500

    Add project files.

---
+++ AdventCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventCode
+{ 
+    internal class Program
+    {
+        static void Main()
+        {
+            //TODO Read input file. Reads each line and adds them to array seperately 
+            String[] lines = File.ReadAllLines(@"C:\Users\jmota\Desktop\C#practice\AdventCode\AdventCode\input.txt");
+
+            int calories = 0;
+            List<int>elf = new List<int>();
+                        
+            foreach (var line in lines)
+            {
+                 if (string.IsNullOrEmpty(line))
+                {
+                    //TODO If there is a null line and add the calories, reset count
+                    elf.Add(calories);
+                    calories = 0;
+                }
+                else
+                {
+                    //covert line to int and add it to the calories in
+                    calories = calories + int.Parse(line);
+                }
+            }
+            Console.WriteLine(elf.Max());
+        }
+    }
+}
